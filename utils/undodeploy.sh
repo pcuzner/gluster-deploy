b28ad81c9becc4fd16476b00cf103635c8fcd341
Paul Cuzner <pcuzner@redhat.com>
2014-01-07 11:59:29 +1300

    Refactored undo script & added purge of ssh keys and hook ready for btrfs wipe

--- utils/undodeploy.sh
+++ utils/undodeploy.sh
@@ -17,101 +17,156 @@
 # Receive the volume to 'drop'
 # Query gluster to get a list of the bricks in the volume
 # Query gluster to get a list of nodes
-# Process each 'foreign' node
+# Query the host to determines the filsystem type of the bricks in the volume
+# Drop the volume (stop and delete)
+# Process each  node
 # 	unmount the brick
-#	drop the associated VG (and LV)
-#   remove the brick from the fstab
-# Repeat above 3 steps for the local node
+#	remove from fstab
+#	handle the brick filesystem
+#		drop the associated VG (and LV)  OR
+#		drop btrfs
 # Drop the foreign nodes from the gluster cluster
 # 
 
 
+function usage {
+	echo -e "\nUsage: $prog -v volume_name"
+	echo -e "This script 'undoes' a configuration defined by the gluster-deploy.py tool."
+	echo -e "\nIt accepts the following arguments;"
+	echo -e "\t-h ... displays this usage information"
+	echo -e "\t-v ... provides the volume name to drop"
+	echo -e "\t-n ... NORUN mode, just displays the commands that would be executed"
+	echo -e "\nNB. The gluster volume must be in a started state for this script to work correctly."
+
+}
+
 
 function unmountFS {
+	local target=$1
+	local cmd="umount $brickPath"
 
-	echo "Unmounting brick filesystems"
-	for node in $nodeList; do
-		echo -e "\tUnmounting $brickPath on node $node"
-		ssh $node umount $brickPath
+	echo -e "\t\tUnmounting brick filesystem on $target"
+	if [ "$target" != "localhost" ] ; then
+		cmd="ssh $target $cmd"
+	fi
+
+	$pfx $cmd	
+	
+}
+
+function fmtCmdOut {
+	# function that receives command output and splits into an array (by \n)
+	# to keep the output formatting sequence correct
+
+	local cmdOutput="$1"			# Needs quotes since string will contain spaces
+
+	readarray -t output <<<"$cmdOutput"
+	
+	for line in "${output[@]}"; do
+		echo -e "\t\t-->$line"
 	done
 }
+	
 
 function dropLVM {
+	local node=$1
+	local conn=""
+	local cmdOut=""
 	
-	echo -e "\nDropping volume group definitions"
-	for node in $nodeList; do 
-		local devList=$(ssh $node pvs | awk "/$vgName/ {print \$1;}")
-		echo -e "- processing node $node"
-		ssh $node vgremove $vgName -f
-		for dev in $devList; do 
-			echo "- removing device $dev from LVM control"
-			ssh $node pvremove $dev
-		done
-			
-	done
+	echo -e "\t\tRemoving LVM definitions(LV and VG)"
+	cmd="pvs | awk '/"$vgName"/ {print \$1;}'"
+
+	if [ "$node" != "localhost" ] ; then 
+		conn="ssh $node "
+		local devList=$($conn $cmd)
+	else
+		local devList=$(pvs | awk "/$vgName/ {print \$1;}")
+	fi
+	
+
+	cmd="vgremove $vgName -f"
+	cmdOut=$($pfx $conn $cmd 2>&1)
+	fmtCmdOut "$cmdOut"
+	
+
+	echo -e "\t\tWiping PV label from device(s)"
+	cmd="pvremove $devList"
+	cmdOut=$($pfx $conn $cmd 2>&1)
+	fmtCmdOut "$cmdOut"
 
 }
+
+function dropBTRFS {
+	local node=$1
+}
+
+
 
 function fstabUpdate {
+	local target=$1
+	local cmd=""
+	local conn=""
 	
-	echo -e "\nRemoving the brick entry from remote fstab's"
-	for node in $nodeList; do 
-	  ssh $node /bin/cp -f /etc/fstab /etc/fstab_saved
-	  
-	  uuid=$(ssh $node blkid $lvName | tr '"' ' '| awk '{print $3;}')
-	  echo -e "- updating /etc/fstab on $node (backup in fstab_saved)"
-	  # ssh $n sed -n -i "/$s/\!p" /root/testfile
-	  ssh $node sed -n -i '/'$uuid'/!p' /etc/fstab 
-	  ssh $node rm -fr "$brickPath"
-	done 
+	if [ "$target" != "localhost" ]; then 
+		conn="ssh $target "
+	fi
 
-}
+	# Create a backup copy of fstab...just in case!
+	cmd="/bin/cp -f /etc/fstab /etc/fstab_saved"
 
-function localUpdates {
+	echo -e "\t\tSaving the current fstab to /etc/fstab_saved"
+	$pfx $conn $cmd 
 	
-	local devList=$(pvs | awk "/$vgName/ {print \$1;}")
-	local uuid=$(blkid $lvName | tr '"' ' '| awk '{print $3;}')
-	
-	echo -e "\nProcessing local node"
-	umount $brickPath
-	echo "- removing volume group $vgName"
-	vgremove $vgName -f
-	
-	for dev in $devList; do
-		echo "- removing device $dev from LVM control"
-		pvremove $dev
-	done
-	
-	echo -e "- Backing up local fstab"
-	/bin/cp -f /etc/fstab /etc/fstab_saved
+	echo -e "\t\tRemoving the fstab entry for $brickPath in fstab"
+	cmd="sed -n -i '\,$brickPath,"'!p'"' /etc/fstab"
 
-	echo -e "- Removing brick entry from fstab"
-	sed -n -i '/'$uuid'/!p' /etc/fstab
-	echo -e "- Removing brick dir root"
-	rm -fr $brickPath
+	# If this is the localhost, run the command directly
+	if [ "$target" == "localhost" ] ; then
+		sed -n -i '\,'$brickPath',!p' /etc/fstab	
+	else
+		# Execute the commands built inside the variables
+		$pfx $conn $cmd
+	fi
+
+	$pfx $conn rm -fr "$brickPath"
+
 }
 
 function dropVolume {
-	echo "Stopping volume $volName..."
-	echo "y" | gluster vol stop $volName force
+	local cmd=""
+	local cmdOut=""
+
+	echo -e "Stopping volume $volName..."
+	echo "y" | gluster vol stop $volName force 
 	
-	echo "Deleting the volume"
 	# This will cause a prompt to the user
-	gluster vol del $volName
+	cmd="gluster vol del $volName"
+	$pfx $cmd 
+
 }
 
 function getNodes {
-	local nodes=$(gluster peer status | awk '/Hostname/ {print $2;}')
+	# Requires glusterfs 3.4+ for the gluster pool list command
+	local nodes=$(gluster pool list | awk '/-/ { print $2; }')
 	echo $nodes
 }
 
-function dropNodes {
+function dropNode {
+	local node=$1
+	local cmd=""
+	local cmdOut=""
 
-	echo -e "\nDropping nodes from the gluster cluster"	
-	for node in $nodeList; do
-		echo "- dropping $node"
-		gluster peer detach $node
-	done
+	if [ "$node" == "localhost" ]; then 
+		return 0
+	fi
+
+	echo -e "\t\tRemoving $node from the cluster"	
+	
+	cmd="gluster peer detach $node"
+
+	cmdOut=$($pfx $cmd 2>&1)
+	fmtCmdOut "$cmdOut"
+
 }
 
 function numItems {
@@ -119,19 +174,57 @@ function numItems {
 	
 }
 
+function removeSSH {
+	local node=$1
+	local cmd=""
+	local conn=""
+
+	# We only have to remove the keys for the remote hosts, so if this is the
+	# local machine, just return to the caller
+	if [ "$node" == "localhost" ] ; then 
+		echo -e "\t\tRemoving ssh keys skipped (no keys to remove on localhost)"
+		return 0
+	fi
+
+	conn="ssh $node"
+
+	echo -e "\t\tRemoving ssh keys"
+	cmd="sed -n -i '/root@$thisHost/"'!p'"' /root/.ssh/authorized_keys"
+
+	# Execute the commands built inside the variables
+	$pfx $conn $cmd
+	
+}
+
 function resetCluster {
 
 	dropVolume || return $?
+
+	echo "Removing node configuration settings..." 
 	
-	unmountFS || return $?
+	for node in $nodeList;  do
+
+		echo -e "\n\tProcessing peer $node"
+
+		unmountFS $node || return $?
+
+		fstabUpdate $node || return $?
+		
+		case "$brickType" in 
+			xfs)
+				dropLVM $node || return $?
+				;;
+			btrfs)	
+				dropBTRFS $node || return $?
+				;;
+ 		esac
+		
+		dropNode $node || return $?
+
+		removeSSH $node || return $?
+
+	done
 	
-	fstabUpdate || return $?
-	
-	dropLVM || return $?
-	
-	localUpdates || return $?
-	
-	dropNodes || return $?
 }
 	
 
@@ -139,17 +232,29 @@ function main {
 	
 	thisHost=$(hostname -s)
 	nodeList=$(getNodes)
+	pfx=""					# pfx is only set in debug mode to show
+						# commands instead of executing them 
 
 	numNodes=$(numItems $nodeList) 
 	
-	while getopts ":v:" OPT; do
+	NORUN=false
+
+	while getopts ":nhv:" OPT; do
 		case "$OPT" in
+			h)
+				usage
+				return 20
+				;;
+			n)
+				NORUN=true
+				;;
 			v)
 				volName=$OPTARG
 				;;
 
 			\?)
 				echo -e "\t- Unknown option provided -$OPTARG"
+				usage
 				return 16
 				;;
 
@@ -159,6 +264,12 @@ function main {
 				;;
 		esac
 	done
+
+	if $NORUN; then 
+		echo -e "\n--> Running in norun mode <--\n"
+		pfx='echo >>> DEBUG - Cmd to execute : '
+	fi
+
 	
 	if [ -z $volName ] ; then 
 		echo -e "\t- not enough parameters supplied"
@@ -185,11 +296,13 @@ function main {
 
 	lvName=$(grep $brickPath /proc/mounts | awk '{ print $1;}')
 	vgName=$(lvs $lvName --noheadings | awk '{print $2;}')
-
+ 	brickType=$(awk -v fs=$brickPath '{ if($2 == fs) { print $3;}}' /proc/mounts)
+	
 	echo -e "Actions to undertake are as follows;"
 	echo -e "\t- drop gluster volume called $volName"
 	echo -e "\t- drop cluster definition ($numNodes nodes)"
-	echo -e "\t- drop associated LVM volume group ($vgName) and all associated PV's"
+	echo -e "\t- drop brick filesystem ($brickType)"
+	echo -e "\t- drop associated LVM volume group ($vgName) and all associated LV's & PV's"
 	echo -e "\t- remove fstab entries for filesystems used for the bricks"
 	
 
@@ -208,8 +321,9 @@ function main {
 	done
 
 	if $goAhead ; then 
-		echo -e "\n\n\tRemoving configuration"
+		
 		resetCluster
+
 	else
 		echo -e "\n\nRun Aborted by user"
 		return 8
@@ -217,7 +331,7 @@ function main {
 	
 }
 
-
-echo -e "\nundodeploy.sh starting"
+prog=$(basename $0)
+echo -e "\n$prog starting"
 main "$@";
-echo -e "\nundodeploy.sh finished - highest RC = $?"
+echo -e "\n$prog finished - highest RC = $?"

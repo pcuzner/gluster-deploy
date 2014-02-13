25fabc5702cc5a5fb23f4c01472d01c61362e6a4
Paul Cuzner <pcuzner@redhat.com>
2014-02-13 13:59:43 +1300

    UI finish summary now the same as the CLI output

--- www/js/finish.js
+++ www/js/finish.js
@@ -6,13 +6,37 @@ function showFinish(state) {
 	showBusy(); // turn off the showbusy message
 
 	if ( state == 'OK') {
-		// activate finish div	
-		slide('volCreate','finish');
+		// invoke the finish call back to the server to get a
+		// summary of what was done
+		
+		var xmlString = "<data><request-type>finish</request-type></data>";
+	
+		xml_http_post('../www/main.html', xmlString, showSummary);
+	
 	}
 	else {
+		
 		//activate the error page
 		slide('volCreate','error');
 		shutDown();
 	}
 
+}
+
+
+function showSummary(req) {
+	
+	var xmlDoc = req.responseXML;
+	var msg = xmlDoc.getElementsByTagName("message");
+	var summary = document.getElementById('tasksummary');
+	var taskSummary = "";
+	
+	for (var i=0; i<msg.length; i++) {
+		taskSummary += "<li class='icon-ok'>" + msg[i].childNodes[0].nodeValue + "</li>"
+	}
+	
+	summary.innerHTML = taskSummary;
+	
+	slide('volCreate','finish');
+	
 }

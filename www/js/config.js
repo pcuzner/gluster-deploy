5ed66962077a99c43fed6c6d0ed36de13ba5b040
Paul Cuzner <pcuzner@redhat.com>
2014-02-13 20:27:56 +1300

    Added var to track the current page in the UI

--- www/js/config.js
+++ www/js/config.js
@@ -13,6 +13,8 @@ var pollingInterval = 1000; 	// ms polling delay used by queryMsgs
 
 var msgLog = new Array();		// array holding status messages from the server 
 
+var currentPage = '';			// string used to track the current page in the UI
+
 /* Brick Object creator Method */
 function Brick(svr, fsname, size) {
 	this.server=svr;

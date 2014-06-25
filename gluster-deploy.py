1be09b7b150f75da7f8bd74e7bba7ae5a202445c
Paul Cuzner <pcuzner@redhat.com>
2014-06-25 17:01:01 +1200

    version bump

--- gluster-deploy.py
+++ gluster-deploy.py
@@ -133,7 +133,7 @@ def main():
 		
 		httpd.server_close()
 
-		cfg.LOGGER.info('%s http server stopped', time.asctime())	
+		cfg.LOGGER.info('%s http server stopped - Now your work starts!', time.asctime())	
 
 	else:
 		print '\n\n-->Problem generating an ssh key, program aborted\n\n'
@@ -149,7 +149,7 @@ if __name__ == '__main__':
 	
 	usageInfo = "usage: %prog [options]"
 	
-	parser = OptionParser(usage=usageInfo,version="%prog 0.7")
+	parser = OptionParser(usage=usageInfo,version="%prog 0.8a")
 	parser.add_option("-n","--no-password",dest="skipPassword",action="store_true",default=False,help="Skip access key checking (debug only)")
 	parser.add_option("-p","--port",dest="port",default=8080,type="int", help="Port to run UI on (> 1024)")
 	parser.add_option("-f","--config-file",dest="cfgFile",type="string",help="Config file providing server list bypassing subnet scan")

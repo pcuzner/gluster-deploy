5899f8b44456af496d49f2c1d7fa17224b76070f
Paul Cuzner <pcuzner@redhat.com>
2014-07-05 16:57:17 +1200

    undodeploy restructured (& simplified) for multi-volume support

--- utils/undodeploy.sh
+++ utils/undodeploy.sh
@@ -16,28 +16,19 @@
 # *                                                             *
 # ***************************************************************
 #
-# Receive the volume to 'drop'
-# Query gluster to get a list of the bricks in the volume
-# Query gluster to get a list of nodes
-# Query the host to determines the filsystem type of the bricks in the volume
-# Drop the volume (stop and delete)
-# Process each  node
-# 	unmount the brick
-#	remove from fstab
-#	handle the brick filesystem
-#		drop the associated VG (and LV)  OR
-#		drop btrfs
-# Drop the foreign nodes from the gluster cluster
-# 
-
+# 1 brick = 1 LV = 1 VG = 1 PV .... nice and (over?) simple!
+#
+# This script must run on the host that initially performed the 'deploy'
+# since it relies on the passwordless ssh that deploy sets up, to drop
+# the gluster configuration
+#
 
 function usage {
-	echo -e "\nUsage: $prog -v volume_name"
-	echo -e "This script 'undoes' a configuration defined by the gluster-deploy.py tool."
+	echo -e "\nUsage: $prog "
+	echo -e "This script will 'undo' a configuration defined by the gluster-deploy.py tool."
 	echo -e "\nIt accepts the following arguments;"
 	echo -e "\t-h ... displays this usage information"
-	echo -e "\t-v ... provides the volume name to drop"
-	echo -e "\t-n ... NORUN mode, just displays the commands that would be executed"
+	echo -e "\t-d ... DEBUG mode, just displays the commands that would be executed"
 	echo -e "\nNB. The gluster volume must be in a started state for this script to work correctly."
 
 }
@@ -45,14 +36,11 @@ function usage {
 
 function unmountFS {
 	local target=$1
-	local volumePath=$2
+	local pathName=$2
 	
-	local cmd="umount $volumePath"
+	local cmd="ssh $target umount $pathName"
 
-	echo -e "\t\tUnmounting brick filesystem on $target"
-	if [ "$target" != $localNode ] ; then
-		cmd="ssh $target $cmd"
-	fi
+	echo -e "\t\tUnmounting filesystem $pathName on $target"
 
 	$pfx $cmd	
 	
@@ -85,40 +73,38 @@ function dropLVM {
 	local vg=$(getVG $node $lv)
 	local pv=$(getPV $node $vg)
 
-	#local pool=$(getPool $node $lv)
+
 	
 	echo -e "\t\tRemoving LVM definitions(LV and VG)"
 	
-	if [ $node != $localNode ]; then 
-		conn="ssh $node"
-	fi
-
-
-	# lvremove /dev/vg/lv --> does not remove the thinpool
-	# lvremove lv --> does remove thinpool and lv
-	# vgremove with -f does BOTH which is what we want!
-
-	cmd="vgremove -f $vg"
-	cmdOut=$($pfx $conn $cmd 2>&1)
+	cmd="ssh $node vgremove -f $vg"
+	cmdOut=$($pfx $cmd 2>&1)
 	rc=$?
 	fmtCmdOut "$cmdOut"
 		
 	if [ $rc -eq 0 ] ; then 
 		echo -e "\t\tWiping PV label from device(s)"
-		cmd="pvremove $pv"
-		cmdOut=$($pfx $conn $cmd)
+		cmd="ssh $node pvremove $pv"
+		cmdOut=$($pfx $cmd)
 		rc=$?
 		fmtCmdOut "$cmdOut"
 	fi
 			
-	#fi
-
 	return $rc
 }
 
-function dropBTRFS {
-	local node=$1
-	# issue a "wipefs -a" against the devices
+function tunedReset {
+	local target=$1
+	
+	echo -e "\t\tResetting tuned profile to 'default' on $target"
+	cmd="ssh $target tuned-adm profile default 2>&1"
+	resp=$($pfx $cmd)
+	rc=$?
+	
+	fmtCmdOut "$resp"
+	
+
+	return $rc
 }
 
 
@@ -128,45 +114,49 @@ function fstabUpdate {
 	local pathName=$2
 	local cmd=""
 	local conn=""
-	#local actualPath=$(dirname $pathName)
 	
-	
-	if [ "$target" != $localNode ]; then 
-		conn="ssh $target "
-	fi
 
 	# Create a backup copy of fstab...just in case!
-	cmd="/bin/cp -f /etc/fstab /etc/fstab_saved"
+	cmd="ssh $target /bin/cp -f /etc/fstab /etc/fstab_saved"
 
 	echo -e "\t\tSaving the current fstab to /etc/fstab_saved"
 	$pfx $conn $cmd 
 	
 	echo -e "\t\tRemoving the fstab entry for $pathName in fstab"
-	cmd="sed -n -i '\,$pathName,"'!p'"' /etc/fstab"
+	cmd="ssh $target sed -n -i '\,$pathName,"'!p'"' /etc/fstab"
 
-	# If this is the localhost, run the command directly
-	if [ "$target" == $localNode ] ; then
-		sed -n -i '\,'$pathName',!p' /etc/fstab	
-	else
-		# Execute the commands built inside the variables
-		$pfx $conn $cmd
-	fi
-
-	$pfx $conn rm -fr "$pathName"
+	$pfx $cmd
+	
+	echo -e "\t\tRemoving directory $pathName on $target"
+	cmd="ssh $target rm -fr $pathName"
+	
+	$pfx $cmd
 
 }
 
 function dropVolume {
+	local volume=$1
 	local cmd=""
 	local cmdOut=""
+	local rc=0
 
-	echo -e "Stopping volume $volName..."
-	echo "y" | gluster vol stop $volName force 
+	echo -e "\t-> Stopping volume '$volName'..."
+	cmd="yes | gluster vol stop $volume force 2>&1"
+	cmdOut=$($pfx eval $cmd )
+	rc=$?
+	fmtCmdOut "$cmdOut"
 	
-	# This will cause a prompt to the user
-	cmd="gluster vol del $volName"
-	$pfx $cmd 
+	if [ $rc -eq 0 ] ; then 
+		# delete the volume
+		echo -e "\t-> Deleting volume '$volName'..."
+		cmd="yes | gluster vol del $volName 2>&1"
+		cmdOut=($pfx eval $cmd)
+		fmtCmdOut "$cmdOut"
+		rc=$?
+	fi
 
+
+	return $rc
 }
 
 
@@ -176,22 +166,19 @@ function dropNode {
 	local cmd=""
 	local cmdOut=""
 
-	if [ "$node" == $localNode ]; then 
+	if [ "$node" == "localhost" ]; then 
+		echo -e "\t\tSkipping peer detach for the local peer"
 		return 0
+	else
+		echo -e "\t\tRemoving $node from the cluster"	
+		
+		cmd="gluster peer detach $node"
+	
+		cmdOut=$($pfx $cmd 2>&1)
+		fmtCmdOut "$cmdOut"		
 	fi
-
-	echo -e "\t\tRemoving $node from the cluster"	
 	
-	cmd="gluster peer detach $node"
 
-	cmdOut=$($pfx $cmd 2>&1)
-	fmtCmdOut "$cmdOut"
-
-}
-
-function numItems {
-	echo $#
-	
 }
 
 function removeSSH {
@@ -201,143 +188,167 @@ function removeSSH {
 
 	# We only have to remove the keys for the remote hosts, so if this is the
 	# local machine, just return to the caller
-	if [ "$node" == $localNode ] ; then 
+	if [ "$node" == "localhost" ] ; then 
 		echo -e "\t\tRemoving ssh keys skipped (not applicable on local node)"
 		return 0
 	fi
-
+	
 	conn="ssh $node"
-
-	echo -e "\t\tRemoving ssh keys"
-	cmd="sed -n -i '/root@$thisHost/"'!p'"' /root/.ssh/authorized_keys"
+	echo -e "\t\tRemoving ssh keys from node $node"
+	cmd="sed -n -i '/root@$node/"'!p'"' /root/.ssh/authorized_keys"
 
 	# Execute the commands built inside the variables
 	$pfx $conn $cmd
 	
 }
 
+function umountVolume {
+	local volName=$1
+	
+	mountState=$(grep $volName /proc/mounts)
+	
+	if [ $? -eq 0 ] ; then 
+		# all mount points will be the same since this is gluster-deploy 
+		# based config
+		
+		local mountPath=$(echo $mountState | cut -d " " -f 2)
+		echo "$volName will be unmounted from all systems"
+		
+		for peer in "${nodeList[@]}"; do 
+			
+			# 1. issue the umount
+			unmountFS $peer $mountPath
+			
+			# 2. update fstab
+			fstabUpdate $peer $mountPath
+		
+			
+		done
+		
+	else
+		echo -e "\t'$volName' is not mounted on the local node, skipping umount logic"
+	fi 
+	
+
+
+}
+
+function getBrickType {
+	local volName=$1
+	local brickType="xfs"
+	
+	brickType=$(gluster vol status $volName detail | grep "File System" | head -n 1 | cut -d ":" -f 2 | sed 's/^ *//')
+	
+	echo $brickType
+}
+
+
 function resetCluster {
 
-	local brickPaths=($@)
-	local lvName=""
+	# flag set by each volume to determine the brick provider
+	# xfs = lvm, or btrfs
+	local brickType='xfs'
 
-	dropVolume || return $?
+	# check for the presense of snapshots - any snapshots will cause the
+	# script to abort
+	if $snapshotsAvailable ; then 
+		local snapList=$(gluster snap list)
+		if [ "$snapList" != "No snapshots present" ]; then
+			echo "undodeploy can not continue - one or more volumes still have snapshots"
+			echo "associated with them."
+			return 12
+		fi 
+	fi
+	
+	local -a brickPaths
 
-	echo "Removing node configuration settings..." 
+	echo -e "\nRemoving volume(s)...prepare for mind-wipe!"
+	
+	# process each volume in volList
+	for volName in "${volList[@]}"; do 
 
-	for brickInfo in "${brickPaths[@]}"; do 
+		# get the type of brick provider used for the volumes bricks
+		brickType=$(getBrickType $volName)
+		echo -e "\nProcessing volume '$volName'"
 
-		IFS=":"
-		set -- $brickInfo
-		local node=$1
+		# unmount the volume if mounted on the nodes		
+		# assumption is that deploy mounts a volume to all nodes, so the 
+		# presence on this node, means it has to be done across all nodes 
+		# and remove any mount setting from fstab 
+		umountVolume $volName || return $?
+				
+		dropVolume $volName || return $?
+	
 
-		local brickPath=$(dirname $2)		
+		
 		unset IFS
-
-		#if [ $node == $localNode ] ; then 
-		#	node='localhost'
-		#fi
-
-		echo -e "\n\tProcessing peer $node"
-
-		# It's important to perform the get lv before the unmount
-		# or we lose the x-ref between mountpoint and device
-		lvName=$(getLV $node $brickPath)
-
-		unmountFS $node $brickPath || return $?
-
-		fstabUpdate $node $brickPath || return $?
+		read -a brickPaths <<< ${vol2Bricks[$volName]}
+		echo -e "\tRemoving bricks"
 		
-		case "$brickType" in 
-			xfs)
-				dropLVM $node $lvName || return $?
-				;;
-			btrfs)	
-				dropBTRFS $node || return $?
-				;;
- 		esac
+		for brickInfo in "${brickPaths[@]}"; do 
+	
+			IFS=":"
+			set -- $brickInfo
+			local node=$1
+	
+			local brickPath=$(dirname $2)		
+			unset IFS
+	
+			echo -e "\t- Processing peer '$node'"
+	
+			# It's important to perform the get lv before the unmount
+			# or we lose the x-ref between mountpoint and device
+			lvName=$(getLV $node $brickPath)
+	
+			unmountFS $node $brickPath || return $?
+	
+			fstabUpdate $node $brickPath || return $?
+			
+			
+			case "$brickType" in 
+				xfs)
+					dropLVM $node $lvName || return $?
+					;;
+				btrfs)	
+					echo "Not ready for btrfs YET!"
+					return 20
+					;;
+	 		esac
+	
+		done
+	done
 
-		let disksProcessed[$node]=${disksProcessed[$node]}+1
+	# At this point each volume has been deleted, bricks unmount, LVM configs
+	# dropped and fstabs updated
+	# Next up is to remove the ssh keys from the other nodes in the cluster
+	echo -e "\n\tResetting tuned, removing ssh keys and peer membership"
+	for peer in "${nodeList[@]}"; do 
 		
-
-
-		# if this is the last brick on this node, we can remove the ssh
-		# keys and drop the node from the gluster config
-		if [ ${disksProcessed[$node]} == ${totalDisks[$node]} ]; then 
-			
-			removeSSH $node || return $?
-			
-			dropNode $node || return $?
-			
-		else
-			echo -e "\t\tBypassing ssh key removal, since more bricks to remove on this node($node)"
-		fi
+		echo -e "\n\tProcessing node $peer"
+		tunedReset $peer || return $?
 		
+		removeSSH $peer || return $?
+		
+		dropNode $peer || return $?
 
 	done
-}
 
-function isName {
-	local hostName=$1
-	local re='^[0-9]'
-	local char1=${hostName:0:1}
-	local result=true
-	
-	if [[ $char1 =~ $re ]]; then 
-			result=false
-	fi
-	
-	echo $result
+
 }
 
 
-
-function isLocalHost {
-	# function receiving a hostname, and returning whether that host
-	# resolves to an IP on the local machine (boolean)
-	
-	local thisNode=$1
-
-	if $(isName $thisNode); then 
-		local target=$(host $thisNode | awk ' { print $4;}')
-	else
-		local target=$thisNode
-	fi
-	
-	local IPonHost=$(ip addr show | grep $target)
-	local resp=true
-
-	if [ -z "$IPonHost" ]; then
+function snapsOK {
+	resp=true
+	local snapPGM=$(rpm -ql glusterfs | grep snap)
+	if [ -z $snapPGM ]; then
 		resp=false
 	fi
 
-	echo $resp
-}
-
-
-
-
-function getLocalBrick {
-
-	# I assume that all the bricks across the cluster are formatted in a consistent 
-	# manner by gluster-deploy, so I just need to find the first brick on the 
-	# local node to determine the fs type...fstype then indicates whether this is
-	# and lvm or btrfs environment
-
-	local brickPaths=($@)
-	for brick in "${brickPaths[@]}"; do
-		IFS=":"
-		set -- $brick
-		node=$1
-		if [ $node == $localNode ]; then
-			local mountPath=$(dirname $2)
-			echo $mountPath
-			break
-		fi
-		unset IFS
-	done
+	echo    $resp
 
 }
+
+
 
 function getLV {
 	# Look in the mount table for a given brick path to find the associated lv
@@ -345,27 +356,8 @@ function getLV {
 	
 	local node=$1
 	local mountPoint=$2
-	#local mountPoint=$(dirname $pathName)
-	
-	if [ $node != $localNode ]; then 
-		ssh $node "awk -v fs=$mountPoint '{ if(\$2 == fs) { print \$1;}}' /proc/mounts"
-	else
-		awk -v fs=$mountPoint '{ if($2 == fs) { print $1;}}' /proc/mounts	
-	fi
 
-}
-
-function getPool {
-	# get the poolname. Using xargs as a simple way to trim leading/trailing whitespace
-	local node=$1
-	local lv=$2
-	
-	if [ $node != $localNode ]; then 
-		ssh $node "lvs $lv --noheading -o pool_lv | xargs"
-	else
-		lvs $lv --noheading -o pool_lv | xargs
-	fi
-
+	ssh $node "awk -v fs=$mountPoint '{ if(\$2 == fs) { print \$1;}}' /proc/mounts"
 }
 
 
@@ -376,91 +368,49 @@ function getPV {
 	
 	local node=$1
 	local vg=$2
-	if [ $node != $localNode ]; then 
-		ssh $node "vgs $vg -o pv_name --noheadings | xargs"	
-	else
-		vgs $vg -o pv_name --noheadings | xargs
-	fi
+	
+	ssh $node "vgs $vg -o pv_name --noheadings | xargs"	
 }
 
 function getVG {
 	local node=$1
 	local lv=$2
 
-	if [ $node != $localNode ] ; then 
-		ssh $node "lvs $lv --noheadings -o vg_name | xargs"
-	else 
-		lvs $lv --noheadings -o vg_name | xargs
-	fi
+	ssh $node "lvs $lv --noheadings -o vg_name | xargs"
+
 }
-
-
-function getNodes {
-	local nodes=$(gluster peer status | awk '/Hostname:/ { print $2; }')
-	echo $nodes "localhost"
-}
-
-function createNodeCounters {
-	# Sets up the associated arrays to track the number of bricks per node
-	# and establishes which node is the localhost (localNode variable)
-	
-	local brickPath=($@)
-
-	IFS=":"
-	for brick in "${brickPath[@]}"; do
-		set -- $brick
-		local node=$1
-		local path=$2
-
-		local chkLocal=$(isLocalHost $node)
-		if [ "$chkLocal" = true ]; then
-			localNode=$node
-		fi
-
-		if [ -z ${totalDisks[$node]} ]; then
-			totalDisks[$node]=1
-			disksProcessed[$node]=0
-		else
-			let totalDisks[$node]=${totalDisks[$node]}+1
-		fi
-
-	done
-	
-	unset IFS
-}
-
 
 
 function main {
 	
 	
-	# Define associative arrays to track the bricks by node for clusters that have 
-	# more than one brick defined in a volume from a given node
-	declare -A totalDisks
-	declare -A disksProcessed
-
-	localNode=''
-	thisHost=$(hostname -s)
-	nodeList=$(getNodes)
-	pfx=""					# pfx is only set in debug mode to show
-						# commands instead of executing them 
-
-	numNodes=$(numItems $nodeList) 
+	# Define associative array to track bricks belonging to a volume
+	declare -A vol2Bricks
 	
-	NORUN=false
+	snapshotsAvailable=$(snapsOK)
+	
+	read -a nodeList <<< $(gluster peer status | awk '/Hostname:/ {print $2;}')
+	nodeList[${#nodeList[*]}]='localhost'
+	
+	pfx=""					# pfx is only set in debug mode to show
+							# commands instead of executing them 
 
-	while getopts ":nhv:" OPT; do
+	numNodes=${#nodeList[@]}
+	
+	DEBUG=false
+
+	while getopts ":dh" OPT; do
 		case "$OPT" in
 			h)
 				usage
 				return 20
 				;;
-			n)
-				NORUN=true
+			d)
+				DEBUG=true
 				;;
-			v)
-				volName=$OPTARG
-				;;
+			#v)
+			#	volName=$OPTARG
+			#	;;
 
 			\?)
 				echo -e "\t- Unknown option provided -$OPTARG"
@@ -475,46 +425,37 @@ function main {
 		esac
 	done
 
-	if $NORUN; then 
-		echo -e "\n--> Running in norun mode <--\n"
+	if $DEBUG; then 
+		echo -e "\n--> Running in debug mode <--\n"
 		pfx='echo >>> DEBUG - Cmd to execute : '
 	fi
 
-	
-	if [ -z $volName ] ; then 
-		echo -e "\t- not enough parameters supplied"
-		return 4
-	fi
 
-	volList=$(gluster vol list 2>&1)
+	unset IFS
+	read -a volList <<< $(gluster vol list 2>&1)
 	
-	#brickList=$(gluster vol status $volName | tr ":" " " | awk '/Brick/ {print $3;}')
-	brickList=$(gluster vol status $volName | awk '/^Brick/ {print $2;}')
-	if [ -z "$brickList" ] ; then
-			echo "-> Volume '$volName' is not available. undodeploy.sh run aborted"
-			exit 12
+	if [ -z "$volList" ]; then 
+		echo "-> No volumes exist in this configuration"
+		exit 12
 	fi
 	
-	# set the arrays to track bricks per node, and establish the node
-	# from the brick list that is the local host
-	createNodeCounters "$brickList"
-	
-	localBrick=$(getLocalBrick "$brickList")
+	allVolumes=${#volList[@]}
+	allBricks=0
 	
 	
-	lvName=$(getLV $localNode $localBrick)
-		
-
-	vgName=$(getVG $localNode $lvName)
-
- 	brickType=$(awk -v fs=$localBrick '{ if($2 == fs) { print $3;}}' /proc/mounts)
+	echo -e "\nAnalysing each volume in the cluster"
+	for volName in "${volList[@]}"; do 
+		vol2Bricks[$volName]=$(gluster vol status $volName | awk '/^Brick/ {print $2;}'); 
+		read -a bricks <<< ${vol2Bricks[$volName]}
+		let allBricks=$allBricks+${#bricks[@]}
+	done
 	
 	echo -e "Actions to undertake are as follows;"
-	echo -e "\t- drop gluster volume called '$volName'"
-	echo -e "\t- drop cluster definition ($numNodes nodes)"
-	echo -e "\t- drop brick filesystem ($brickType)"
-	echo -e "\t- drop associated LVM volume group(s) and all associated LV's & PV's"
-	echo -e "\t- remove fstab entries for filesystems used by the bricks"
+	echo -e "\t- stop and delete $allVolumes gluster volume(s)"
+	echo -e "\t- unmount and delete $allBricks brick configurations"
+	echo -e "\t- drop the trusted pool ($numNodes nodes)"
+	echo -e "\t- remove all associated LVM definitions for the bricks"
+	echo -e "\t- remove fstab entries for filesystems used by the bricks and volumes"
 
 	while true; do
 		read -r -p "Are you Sure (y/n)?" confirm
@@ -528,13 +469,10 @@ function main {
 		esac
 		echo ""
 	done
-
+	
 	if $goAhead ; then 
 
-
-
-		
-		resetCluster "$brickList"
+		resetCluster ||  return $?
 
 	else
 		echo -e "\n\nRun Aborted by user"
